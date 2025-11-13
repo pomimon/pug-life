451dbe3dc7f90d28c73db106e086540d4b62e012
pomimon <bright.dusk1649@fastmail.com>
2025-11-13 14:29:39 -0800

    Add functions

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,46 @@
 import "./../index.css";
 import React from "react";
+import pugIcon from "./../assets/wallpaper.png";
+import { Header, Wallpaper } from "./Components";
+
+function Desktop() {
+  return (
+    <div className="desktop">
+      <div className="desktop-item">
+        <img src={pugIcon} className="desktop-icon" alt="Pug Icon" />
+        <span className="icon-text">no click</span>
+      </div>
+      <div className="desktop-item">
+        <img src={pugIcon} className="desktop-icon" alt="Pug Icon" />
+        <span className="icon-text">click</span>
+      </div>
+      <div className="desktop-item">
+        <img src={pugIcon} className="desktop-icon" alt="Pug Icon" />
+        <span className="icon-text">no clicky</span>
+      </div>
+      <div className="desktop-item">
+        <img src={pugIcon} className="desktop-icon" alt="Pug Icon" />
+        <span className="icon-text">no click</span>
+      </div>
+      <div className="desktop-item">
+        <img src={pugIcon} className="desktop-icon" alt="Pug Icon" />
+        <span className="icon-text">no click</span>
+      </div>
+      <div className="desktop-item">
+        <img src={pugIcon} className="desktop-icon" alt="Pug Icon" />
+        <span className="icon-text">no click</span>
+      </div>
+      <div className="desktop-item">
+        <img src={pugIcon} className="desktop-icon" alt="Pug Icon" />
+        <span className="icon-text">no click</span>
+      </div>
+      <div className="desktop-item">
+        <img src={pugIcon} className="desktop-icon" alt="Pug Icon" />
+        <span className="icon-text">no click</span>
+      </div>
+    </div>
+  );
+}
 
 export function App() {
   return (
